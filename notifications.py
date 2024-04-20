965feff464c2a16b4ed0ca25e508f0603e976174
simonl169 <98774388+simonl169@users.noreply.github.com>
2024-04-20 18:24:16 +0200

    Develop/feature notifications (#13)
    * Changed DynDNS registrar to Cloudflare
    Updated some functions
    Rewrite to better add multiple domains
    New Logo
    
    * Updated deps
    
    * Updated Readme.md
    
    * Update README.md
    
    Added section on how to obtain Cloudflare credentials and IDs
    
    * Added new feature for push notifications
    Added start-up sequence
    Refactor code into more files
    
    * Change image to Python 3.12

---
+++ notifications.py
@@ -0,0 +1,32 @@
+import requests
+from config import load_config
+
+
+class Notifier:
+    def __init__(self):
+        self.data = load_config('config.json')
+        self.ntfy_server = self.data['NOTIFY_SERVER']
+
+    def send_success(self, message: str = "Generic success message"):
+        message = "🟢 Success: " + message
+        requests.post(self.ntfy_server, data=message.encode(encoding='utf-8'))
+
+    def send_warning(self, message: str = "Generic warning message"):
+        message = "⚠️ Warning: " + message
+        requests.post(self.ntfy_server, data=message.encode(encoding='utf-8'))
+
+    def send_error(self, message: str = "Generic error message"):
+        message = "🚩 Error: " + message
+        requests.post(self.ntfy_server, data=message.encode(encoding='utf-8'))
+
+
+if __name__ == "__main__":
+
+    test_notifier = Notifier()
+    test_notifier.send_success()
+    test_notifier.send_warning()
+    test_notifier.send_error()
+
+    # test_notifier = None
+    # if test_notifier:
+    #     print('ladida')
