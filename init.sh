ef80c0f8feb2df177babba615bb41ff06827d07f
simonl169 <98774388+simonl169@users.noreply.github.com>
2024-05-05 16:44:48 +0200

    Fix error that would accumulate crontab entries (#16)
    Changes init.sh to overwrite mycron file everytime instead of adding entries to it.
    This fixes accumulating duplicates in the crontab file and spamming of the dns updates to the server.
    Fixes #15

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 python -c 'from owl import *; starting_message()' # Run starting message and logo
 python main.py # Run Update once directly at the start
-echo "$CRONVARS2" "cd /app && python main.py" >> mycron
+echo "$CRONVARS2" "cd /app && python main.py" > mycron
 crontab mycron
 crond -f
